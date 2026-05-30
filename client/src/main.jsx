fc349f06ed060f6ba6d1cfe9a1ba9ab39ea7cb4b
Zeeshan <zk45681@gmail.com>
2026-05-30 19:58:25 +0530

    Move security script to React entry point

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,25 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './App.css'
 
+// Disable Right Click (Context Menu)
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+// Disable DevTools shortcuts (F12, Ctrl+Shift+I, Ctrl+Shift+J, Ctrl+U, Ctrl+Shift+C)
+document.addEventListener('keydown', function(event) {
+  // F12
+  if (event.key === 'F12' || event.keyCode === 123) {
+    event.preventDefault();
+  }
+  // Ctrl+Shift+I / Ctrl+Shift+J / Ctrl+Shift+C
+  if (event.ctrlKey && event.shiftKey && (event.key === 'I' || event.key === 'i' || event.key === 'J' || event.key === 'j' || event.key === 'C' || event.key === 'c')) {
+    event.preventDefault();
+  }
+  // Ctrl+U (View Source)
+  if (event.ctrlKey && (event.key === 'U' || event.key === 'u')) {
+    event.preventDefault();
+  }
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
